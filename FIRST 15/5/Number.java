deac039b81cd84df68fe825bbd8e406a90ac1e84
Ahm36 <67476812+Ahm36@users.noreply.github.com>
2021-05-26 12:00:26 +0530

    Add files via upload

---
+++ FIRST 15/5/Number.java
@@ -0,0 +1,11 @@
+class Number{
+	public static void main(String args[]){
+		int a=5;
+		if (a<0)
+			System.out.println("Negative Number");
+		else
+			System.out.println("Positive Number");
+			
+	}
+
+}
