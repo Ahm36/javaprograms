9b076ea224dd34090e1919be2a0d6bf56cc92865
Ahm36 <67476812+Ahm36@users.noreply.github.com>
2021-05-26 12:13:51 +0530

    Add files via upload

---
+++ FIRST 15/6/Greatest.java
@@ -0,0 +1,14 @@
+class Greatest{
+public static void main(String args[])
+{
+	int a=10,b=20,c=30;
+	if(a>b && a>c)
+		System.out.println(a+ " is the largest number");
+	else if(b>c && b>a)
+		System.out.println(b+ " is the largest number");
+	else
+		System.out.println(c+ " is the largest number");
+}
+}
+
+		
