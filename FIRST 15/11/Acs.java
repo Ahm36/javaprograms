fc0fbd8342edf7ade59e79f9e98300f2381bb141
Ahm36 <67476812+Ahm36@users.noreply.github.com>
2021-05-27 09:01:57 +0530

    Add files via upload

---
+++ FIRST 15/11/Acs.java
@@ -0,0 +1,9 @@
+public class Acs {
+  public static void main(String[] args) {
+    int[] num = {13,54,76,98};
+    System.out.println("First element: "+num[0]);
+    System.out.println("Second element: "+num[1]);
+    System.out.println("Third element: "+num[2]);
+    System.out.println("Fourth element: "+num[3]);
+  }
+}
