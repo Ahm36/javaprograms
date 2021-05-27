aa86b39039bcadd9a5eae17adeb658352f5838c3
Ahm36 <67476812+Ahm36@users.noreply.github.com>
2021-05-27 08:49:46 +0530

    Add files via upload

---
+++ FIRST 15/8/Avg.java
@@ -0,0 +1,9 @@
+public class Avg{
+  public static void main(String[] args) {
+    int a=10,b=18,c=21,d=7,e=15,sum,average;
+    sum=a+b+c+d+e;
+    average=sum/5;
+    System.out.println("Sum= " +sum);
+    System.out.println("Average= "+average);
+     }  
+  }
