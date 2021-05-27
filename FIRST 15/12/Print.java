24b274c7c427b76b51bf077dfc0bd08f7305177f
Ahm36 <67476812+Ahm36@users.noreply.github.com>
2021-05-27 09:05:44 +0530

    Add files via upload

---
+++ FIRST 15/12/Print.java
@@ -0,0 +1,9 @@
+public class Print {
+    public static void main(String[] args) {
+        int a[]={13,56,76,89};
+        for (int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+
+    }
+  }
